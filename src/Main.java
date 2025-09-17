a240758922a3a3800316fe997fb34eebe4f02624
Jonathan Rubensson <jonathan@eldfluga.se>
2025-09-17 16:56:08 +0200

    cleans whitespace and removes redundant variable declaration

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
 public class Main {
     public static void main(String[] args){
-        Store store = Store.getInstance();
-        store.startShopping();
-
-
+        Store.getInstance().startShopping();
     }
 }
