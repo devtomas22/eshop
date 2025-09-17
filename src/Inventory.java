9d4100e4503bfe1a5a502636ecd9b2641bfc5722
Tomas <bauds.ringed2n@icloud.com>
2025-09-17 13:51:49 +0000

    remove duplicate line

--- src/Inventory.java
+++ src/Inventory.java
@@ -11,7 +11,6 @@ public class Inventory {
     public Map<String, Product> getProductsInInventory() {
         return Collections.unmodifiableMap(this.products);
     }
-    private static Inventory singleObject;
 
     private Product createProduct(String productName, double price, String productDescription, int numberOfItemsInStock) {
         if (this.products.containsKey(productName)) {
